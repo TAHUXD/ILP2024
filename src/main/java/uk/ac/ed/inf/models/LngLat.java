576897f881ff899ae602ccaf62360964f3fc9296
WilfredoD.N <s2172881@ed.ac.uk>
2024-09-22 00:19:17 +0100

    Added Controller and Exception Package with potential classes

---
+++ src/main/java/uk/ac/ed/inf/models/LngLat.java
@@ -0,0 +1,12 @@
+package uk.ac.ed.inf.models;
+
+public class LngLat {
+    private double lng;
+    private double lat;
+
+    // Getters and setters
+    public double getLng() { return lng; }
+    public void setLng(double lng) { this.lng = lng; }
+    public double getLat() { return lat; }
+    public void setLat(double lat) { this.lat = lat; }
+}
---
+++ src/main/java/uk/ac/ed/inf/models/Position.java
@@ -0,0 +1,12 @@
+package uk.ac.ed.inf.models;
+
+public class Position {
+    private LngLat position1;
+    private LngLat position2;
+
+    // Getters and setters
+    public LngLat getPosition1() { return position1; }
+    public void setPosition1(LngLat position1) { this.position1 = position1; }
+    public LngLat getPosition2() { return position2; }
+    public void setPosition2(LngLat position2) { this.position2 = position2; }
+}
